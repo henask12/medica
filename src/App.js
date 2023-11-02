3c8e884e862e585979537d17108b0628c5e6ccf1
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-11-02 09:13:10 -0300

    Add routes for testing the dr lists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import DoctorList from './components/DoctorList';
+import mockDoctors from './data/mockDoctorsData'; 
 
 function App() {
+
+  const handleDelete = (id) => {
+    console.log('Doctor with ID', id, 'will be deleted.');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/doctors">Doctors List</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<h2>Home Page</h2>} />
+          <Route path="/doctors" element={<DoctorList doctors={mockDoctors} onDelete={handleDelete} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
