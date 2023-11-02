14016a7b325d214a0cff0dd3cae53a9800571089
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-11-02 16:59:31 +0300

    Add route for log in page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import './App.css';
 import Auth from './components/auth/auth';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ForgotPassword from './components/auth/ForgotPassword';
+import SignIn from './components/auth/SignIn';
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/forgot-password" element={<ForgotPassword/>} />
-          <Route path="/" element={<Auth/>} />
-        </Routes>
-      </div>
-    </Router>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/SignIn" element={<SignIn />} />
+            <Route path="/" element={<Auth />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
