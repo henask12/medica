4340c9bf0b2b642506be1f696e3e55d4cb3d4921
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-11-02 09:12:41 -0300

    Create a mock data

---
+++ src/data/mockDoctorsData.js
@@ -0,0 +1,12 @@
+const mockDoctors = [
+  { id: 1, name: 'Dr. Smith', specialty: 'Cardiology', city: 'Madrid', description: 'Expert in cardiovascular diseases', image: 'path/to/image1.jpg' },
+  { id: 2, name: 'Dr. Johnson', specialty: 'Neurology', city: 'Barcelona', description: 'Specializes in nervous system disorders', image: 'path/to/image2.jpg' },
+  { id: 3, name: 'Dr. Williams', specialty: 'Orthopedics', city: 'Seville', description: 'Expert in musculoskeletal system treatments', image: 'path/to/image3.jpg' },
+  { id: 4, name: 'Dr. Garcia', specialty: 'Dermatology', city: 'Valencia', description: 'Skin care specialist with over 20 years of experience', image: 'path/to/image4.jpg' },
+  { id: 5, name: 'Dr. Rodriguez', specialty: 'Pediatrics', city: 'Bilbao', description: 'Child health specialist with a focus on neonatal care', image: 'path/to/image5.jpg' },
+  { id: 6, name: 'Dr. Perez', specialty: 'Endocrinology', city: 'Malaga', description: 'Expert in hormone-related diseases and conditions', image: 'path/to/image6.jpg' },
+  { id: 7, name: 'Dr. Martin', specialty: 'Gastroenterology', city: 'Zaragoza', description: 'Specializes in digestive system and its disorders', image: 'path/to/image7.jpg' },
+  { id: 8, name: 'Dr. Lopez', specialty: 'Pulmonology', city: 'Murcia', description: 'Lung and respiratory system specialist', image: 'path/to/image8.jpg' }
+];
+
+export default mockDoctors;
