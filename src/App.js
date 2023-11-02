34d16806d97b54443df3fef81863149485221c8c
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-11-02 15:17:13 -0300

    Remove delete button for implementing later

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import DoctorList from './components/DoctorList';
 import mockDoctors from './data/mockDoctorsData'; 
-import handleDelete from './components/DeleteButton';
 
 function App() {
-  const handleDelete = (id) => {
-    console.log('Doctor with ID', id, 'will be deleted.');
-  };
   return (
     <Router>
       <div className="App">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">Home</Link>
-            </li>
             <li className="nav-item">
               <Link to="/doctors" className="nav-link">Doctors List</Link>
             </li>
@@ -23,8 +16,7 @@ function App() {
         </nav>
 
         <Routes>
-          <Route path="/" element={<h2>Home Page</h2>} />
-          <Route path="/doctors" element={<DoctorList doctors={mockDoctors} onDelete={handleDelete} />} />
+          <Route path="/doctors" element={<DoctorList doctors={mockDoctors} />} />
         </Routes>
 
       </div>
