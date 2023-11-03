3015820344a302f1b680fb080e83f25120e74893
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-11-04 00:35:07 +0300

    auth update

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -1,47 +1,45 @@
+// Auth.js
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 import "./styles.css";
 import SignUpForm from "./SignUp";
 import SignInForm from "./SignIn";
 
 export default function Auth() {
+  const navigate = useNavigate(); // Initialize navigate
   const [type, setType] = useState("signIn");
-  const handleOnClick = text => {
+
+  const handleOnClick = (text) => {
     if (text !== type) {
       setType(text);
-      return;
     }
   };
-  const containerClass =
-    "container " + (type === "signUp" ? "right-panel-active" : "");
+
+  const handleSignIn = () => {
+    // Assume successful sign-in
+    navigate("/sidebar"); // Use navigate to navigate to the sidebar
+  };
+
+  const containerClass = "container " + (type === "signUp" ? "right-panel-active" : "");
+
   return (
     <div className="App">
       <h2>24 Hour Doctor Auth Page</h2>
       <div className={containerClass} id="container">
-        <SignUpForm />
-        <SignInForm />
+        {type === "signUp" ? <SignUpForm /> : <SignInForm onSignIn={handleSignIn} />}
         <div className="overlay-container">
           <div className="overlay">
             <div className="overlay-panel overlay-left">
               <h1>Welcome Back!</h1>
-              <p>
-                To keep connected with us please login with your personal info
-              </p>
-              <button
-                className="ghost"
-                id="signIn"
-                onClick={() => handleOnClick("signIn")}
-              >
+              <p>To keep connected with us please login with your personal info</p>
+              <button className="ghost" id="signIn" onClick={() => handleOnClick("signIn")}>
                 Sign In
               </button>
             </div>
             <div className="overlay-panel overlay-right">
               <h1>Hello, Friend!</h1>
-              <p>Enter your personal details and start journey with us</p>
-              <button
-                className="ghost "
-                id="signUp"
-                onClick={() => handleOnClick("signUp")}
-              >
+              <p>Enter your personal details and start the journey with us</p>
+              <button className="ghost" id="signUp" onClick={() => handleOnClick("signUp")}>
                 Sign Up
               </button>
             </div>
