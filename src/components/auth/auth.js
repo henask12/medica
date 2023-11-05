77cc0fe848eeac78a81d4966380f685927a8796e
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-11-05 15:43:42 +0300

    update Auth file

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -1,13 +1,12 @@
-// Auth.js
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom"; // Import useNavigate
-import "./styles.css";
-import SignUpForm from "./SignUp";
-import SignInForm from "./SignIn";
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import './styles.css';
+import SignUpForm from './SignUp';
+import SignInForm from './SignIn';
 
-export default function Auth() {
-  const navigate = useNavigate(); // Initialize navigate
-  const [type, setType] = useState("signIn");
+function Auth() {
+  const navigate = useNavigate();
+  const [type, setType] = useState('signIn');
 
   const handleOnClick = (text) => {
     if (text !== type) {
@@ -17,29 +16,29 @@ export default function Auth() {
 
   const handleSignIn = () => {
     // Assume successful sign-in
-    navigate("/sidebar"); // Use navigate to navigate to the sidebar
+    navigate('/sidebar');
   };
 
-  const containerClass = "container " + (type === "signUp" ? "right-panel-active" : "");
+  const containerClass = 'container ' + (type === 'signUp' ? 'right-panel-active' : '');
 
   return (
     <div className="App">
       <h2>24 Hour Doctor Auth Page</h2>
       <div className={containerClass} id="container">
-        {type === "signUp" ? <SignUpForm /> : <SignInForm onSignIn={handleSignIn} />}
+        {type === 'signUp' ? <SignUpForm /> : <SignInForm onSignIn={handleSignIn} />}
         <div className="overlay-container">
           <div className="overlay">
             <div className="overlay-panel overlay-left">
               <h1>Welcome Back!</h1>
-              <p>To keep connected with us please login with your personal info</p>
-              <button className="ghost" id="signIn" onClick={() => handleOnClick("signIn")}>
+              <p>To keep connected with us, please log in with your personal info</p>
+              <button className="ghost" id="signIn" onClick={() => handleOnClick('signIn')}>
                 Sign In
               </button>
             </div>
             <div className="overlay-panel overlay-right">
               <h1>Hello, Friend!</h1>
               <p>Enter your personal details and start the journey with us</p>
-              <button className="ghost" id="signUp" onClick={() => handleOnClick("signUp")}>
+              <button className="ghost" id="signUp" onClick={() => handleOnClick('signUp')}>
                 Sign Up
               </button>
             </div>
@@ -49,3 +48,5 @@ export default function Auth() {
     </div>
   );
 }
+
+export default Auth;
