16cb8e1f582e181ac5ec16e5a9c209c72e4eb02e
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-11-07 23:34:43 +0300

    small fix

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
-
-import logo from './logo.svg';
 import './App.css';
 import Auth from './components/auth/auth';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ForgotPassword from './components/auth/ForgotPassword';
-import Sidebar from './components/navigation/Sidebar'
+import Sidebar from './components/navigation/Sidebar';
 import SignIn from './components/auth/SignIn';
-import logo from './logo.svg';
-import './App.css';
-
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
 import DoctorList from './components/doc/DoctorList';
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/forgot-password" element={<ForgotPassword/>} />
-          <Route path="/sidebar" element={<Sidebar />} />
-          <Route path="/" element={<Auth/>} />
-        </Routes>
-      </div>
-    </Router>
       <Router>
-        <div>
-          <Routes>
-            <Route path="/SignIn" element={<SignIn />} />
-            <Route path="/" element={<Auth />} />
-          <Routes>
-            
+        <Routes>
+          <Route path="/" element={<Auth />}>
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/sidebar" element={<Sidebar />} />
+            <Route path="/signin" element={<SignIn />} />
             <Route path="/doctors" element={<DoctorList />} />
-          </Routes>
-        </div>
+          </Route>
+        </Routes>
       </Router>
     </div>
   );
--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,11 +1,7 @@
-
-import React from "react";
 import { Link, Route, Switch } from 'react-router-dom';
-import { useNavigate } from "react-router-dom"; // Import useNavigate
+import { useNavigate } from "react-router-dom";
 import React, { useState } from "react";
-import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { loginStart, loginSuccess, loginFailure } from '../redux/authSlice';
 
 
 function SignInForm() {
@@ -42,20 +38,6 @@ function SignInForm() {
       });
     }
 
-    // Dispatch the loginStart action to indicate the login process has started
-    dispatch(loginStart());
-
-    // Simulate API request (replace with actual fetch to your Rails API)
-    setTimeout(() => {
-      if (email === 'example@email.com' && password === 'password') {
-        // Dispatch the loginSuccess action with user data upon successful login
-        dispatch(loginSuccess({ email: email }));
-      } else {
-        // Dispatch the loginFailure action with an error message upon a failed login
-        dispatch(loginFailure('Invalid email or password'));
-      }
-    }, 1000);
-
     // Clear the form fields
     setState({
       email: "",
@@ -86,7 +68,6 @@ function SignInForm() {
         <button id="signIn" onClick={() => handleSignIn("signIn")}>
                 Sign In
         </button>
-        <button type="submit">Sign In</button>
       </form>
     </div>
   );
