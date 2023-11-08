49f4f999ba8435c4a2808066472ab8a5bbed0b43
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-11-08 18:40:09 -0300

    Update dr service

--- src/services/doctorsService.js
+++ src/services/doctorsService.js
@@ -21,4 +21,35 @@ doctorsService.fetchDoctors = function () {
     });
 };
 
+// Doctor delete
+
+doctorsService.deleteDoctor = function (doctorId) {
+  return axios
+    .delete(`${API_BASE_URL}/doctors/${doctorId}`)
+    .then((response) => {
+      console.log('Delete doctor response:', response);
+      return response;
+    })
+    .catch((error) => {
+      console.error('Delete doctor error:', error);
+      throw error;
+    });
+};
+
+// Doctor add
+
+doctorsService.addDoctor = function (doctorData) {
+ 
+  return axios.post(`${API_BASE_URL}/doctors`, doctorData)
+  .then(response => {
+    console.log('Add doctor response:', response);
+    return response;
+  })
+  .catch(error => {
+    console.error('Add doctor error:', error);
+    
+    throw error;
+  });
+};
+
 export default doctorsService;
