411d5a7490fca1d7dc97114b61c7e10f52ac39aa
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-11-09 17:28:39 +0300

    updated the app js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,28 @@ import DoctorList from './components/doc/DoctorList';
 import Signup from './components/user/SignUp';
 import { useSelector } from 'react-redux';
 import Signin from './components/user/SignIn';
+import ReservationList from './components/reservation/ReservationList';
 
 function App() {
   const isAuthenticated = useSelector((state) => state.users.isAuthenticated);
+  const storedEmail = localStorage.getItem('email');
+
   return (
     <div className="App">
       <Router>
         <Routes>
-        {isAuthenticated ? (
+        {(isAuthenticated || storedEmail) ? (
           <>
             <Route path="/" element={
             <>
               <Sidebar />
               <DoctorList />
             </>} />
-            <Route path="/sidebar" element={<Sidebar />} />
+            <Route path="/sidebar" element={
+            <>
+              <Sidebar />
+              <ReservationList />
+            </>} />
           </>
         ) : 
           <>
