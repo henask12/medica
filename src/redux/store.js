a5b674e2fb9749f277ac8910ee578ff944ae7720
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-11-09 17:29:23 +0300

    updated store js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import usersReducer from './usersSlice';
 import doctorsReducer from './doctors/doctorsSlice';
+import reservationReducer from './reservation/reservationSlice';
 
 const store = configureStore({
   reducer: {
     users: usersReducer,
-      doctors: doctorsReducer 
+    doctors: doctorsReducer,
+    reservation: reservationReducer,
   },
 });
 
