24cb4bda4a41ab06eed4cd9cd32305fa9360589b
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-11-09 20:32:57 -0300

    New component for dialog

---
+++ src/components/doc/AlertDialogSlide.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function AlertDialogSlide({ open, handleClose, handleConfirm, message }) {
+  return (
+    <Dialog
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <DialogTitle>{"Confirm Action"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">
+          {message}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleConfirm}>Confirm</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+
+AlertDialogSlide.propTypes = {
+  open: PropTypes.bool.isRequired, // open debe ser un booleano y es requerido
+  handleClose: PropTypes.func.isRequired, // onClose debe ser una función y es requerida
+  handleConfirm: PropTypes.func.isRequired, // onDisagree debe ser una función y es requerida
+  message: PropTypes.func.isRequired, // onAgree debe ser una función y es requerida
+};
+
