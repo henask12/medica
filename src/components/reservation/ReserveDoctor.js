392774ac0d3656161fa1c589b398d4ad08aecbaa
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-11-10 00:02:54 +0300

    Reserve doctor page update

--- src/components/reservation/ReserveDoctor.js
+++ src/components/reservation/ReserveDoctor.js
@@ -6,24 +6,27 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchDoctors } from '../../redux/doctors/doctorsSlice';
 import { useState } from 'react';
+import { LabelImportantOutlined } from '@mui/icons-material';
 
 const Container = styled('div')({
   display: 'flex',
   justifyContent: 'center',
-  marginTop: '4rem',
+  alignItems: 'center',
+  height: '80vh',
 });
 
 const StyledCard = styled(Card)({
   display: 'flex',
+  flexWrap: 'wrap',
   maxWidth: '800px',
   margin: '0 auto',
+  className: 'shadow-lg rounded-lg',
 });
 
 const Image = styled(CardMedia)({
   width: '50%',
   objectFit: 'cover',
-  borderTopLeftRadius: '20px',
-  borderBottomLeftRadius: '20px',
+  className: 'rounded-l-lg',
 });
 
 const FormContainer = styled(CardContent)({
@@ -31,43 +34,40 @@ const FormContainer = styled(CardContent)({
   flexDirection: 'column',
   justifyContent: 'center',
   padding: '1rem',
+  width: '50%',
 });
 
 const FormField = styled(TextField)({
   marginBottom: '1rem',
+  className: 'mb-4',
 });
 
 const ReserveButton = styled(Button)({
   marginTop: '2rem',
-  backgroundColor: "#8dd431"
+  className: 'bg-yellow-500 text-white hover:bg-green-600',
+  backgroundColor: "#a3e635"
 });
 
 function DoctorDetails() {
-    debugger
   const { id } = useParams();
 
   const [doctor, setDoctor] = useState(null);
   const dispatch = useDispatch();
-  const { doctors, isLoading, error } = useSelector((state) => state.doctors); 
+  const { doctors, isLoading, error } = useSelector((state) => state.doctors);
 
   useEffect(() => {
-          dispatch(fetchDoctors());
-    }, [dispatch]); 
+    dispatch(fetchDoctors());
+  }, [dispatch]);
 
-
-    useEffect(() => {
-        debugger
-        if (doctors) {
-          const matchedDoctor = doctors.find((doc) => doc?.id === parseInt(id));
-          setDoctor(matchedDoctor);
-        }
-      }, [doctors, id]);
-    
-const [isMouseEntered, setIsMouseEntered] = useState(false);
-
+  useEffect(() => {
+    if (doctors) {
+      const matchedDoctor = doctors.find((doc) => doc?.id === parseInt(id));
+      setDoctor(matchedDoctor);
+    }
+  }, [doctors, id]);
 
   const handleReserveClick = () => {
-    
+    // Handle reservation click here
   };
 
   return (
@@ -79,14 +79,13 @@ const [isMouseEntered, setIsMouseEntered] = useState(false);
           alt={doctor?.name}
         />
         <FormContainer>
+          <h2 className="text-2xl font-semibold mb-4">Doctor Details</h2>
           <FormField
             label="Name"
             variant="outlined"
             value={doctor?.name || "N/A"}
             InputProps={{
-                readOnly: true,
-                onMouseEnter: () => setIsMouseEntered(true),
-                onMouseLeave: () => setIsMouseEntered(false),
+              readOnly: true,
             }}
           />
           <FormField
@@ -94,16 +93,12 @@ const [isMouseEntered, setIsMouseEntered] = useState(false);
             variant="outlined"
             value={doctor?.specialty || "N/A"}
             InputProps={{
-                readOnly: true,
-                onMouseEnter: () => setIsMouseEntered(true),
-                onMouseLeave: () => setIsMouseEntered(false),
+              readOnly: true,
             }}
-
           />
           <Link to={`/reservation/${id}`}>
             <ReserveButton
               variant="contained"
-              color="primary"
               onClick={handleReserveClick}
             >
               Reserve
