591f8104de258644b22316965a4386d4a9e121fa
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-11-10 02:30:23 +0300

    update the reservation service

--- src/services/reservationService.js
+++ src/services/reservationService.js
@@ -5,20 +5,84 @@ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 const reservationService = {};
 
 reservationService.geByUserId = async function (user_id) {
-    return axios
-      .get(`${API_BASE_URL}/reservations?user_id=${user_id}`, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      .then((response) => {
-        console.log(response.data);
-        return response;
-      })
-      .catch((error) => {
-        throw error;
-      });
+  return axios
+    .get(`${API_BASE_URL}/reservations?user_id=${user_id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+      return response;
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+reservationService.createReservation = async function (reservationData) {
+  return axios
+    .post(`${API_BASE_URL}/reservations`, reservationData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+      return response;
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+reservationService.updateReservation = async function (reservationId, reservationData) {
+  debugger
+  return axios
+    .put(`${API_BASE_URL}/reservations/${reservationId}`, reservationData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+      return response;
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+reservationService.showReservation = async function (reservationId) {
+  return axios
+    .get(`${API_BASE_URL}/reservations/${reservationId}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+      return response;
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+reservationService.deleteReservation = async function (reservationId) {
+  return axios
+    .delete(`${API_BASE_URL}/reservations/${reservationId}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+      return response;
+    })
+    .catch((error) => {
+      throw error;
+    });
 };
 
 export default reservationService;
-  
