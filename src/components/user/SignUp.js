6cce4132f015f3296c6b94659089a681679a5b03
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-11-10 10:31:02 +0300

    added logo

--- src/components/user/SignUp.js
+++ src/components/user/SignUp.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { createUser } from '../../redux/usersSlice';
+import logo from '../../assets/logoTwo.png';
 
 const Signup = () => {
   const [formData, setFormData] = useState({
@@ -105,6 +106,7 @@ const Signup = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="bg-white shadow-lg rounded-lg p-4 max-w-md w-full">
+      <img src={logo} alt="Logo" className="mx-auto mb-4 h-22 w-66" />
         <h3 className="text-2xl text-center font-semibold mb-4">Sign Up</h3>
         {successMessage && (
           <div className="bg-green-200 text-green-800 border p-2 rounded-md mb-4">
